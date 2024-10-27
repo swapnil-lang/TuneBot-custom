fbb82d3e14cd900b0497c7d3af0aa3e8348c3a7e
Micky <60691199+AliMickey@users.noreply.github.com>
2024-10-27 16:20:12 +1100

    test reconnection options

--- app/app.py
+++ app/app.py
@@ -71,6 +71,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
     }
     
     ffmpeg_options = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
         'options': '-vn -loglevel warning'
     }
     
